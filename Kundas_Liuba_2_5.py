4849e798ecd19e2d99e1af764545d9a2c9e82102
Liuba-K <67833767+Liuba-K@users.noreply.github.com>
2021-11-26 02:06:37 +0300

    Update Kundas_Liuba_2_5.py
    modified task 5 to lesson 2

--- Kundas_Liuba_2_5.py
+++ Kundas_Liuba_2_5.py
@@ -1,19 +1,30 @@
 price_of_goods_list = input("Введите список цен на товары, копейки вводить через точку")
 
-new_price = ''
-for i in price_of_goods_list.split():
-    if len(i.split(".")) < 2:
-        ruble = i.split(".")[0]
-        new_price += f'{ruble} руб '
-
+new_prices = []
+for price in price_of_goods_list.split():
+    price_parts = price.split(".")
+    if len(price_parts) < 2:
+        ruble = price_parts[0]
+        kopeck = "00"
     else:
-        for idx, val in enumerate(i.split(".")):
-            ruble, kopeck = i.split(".")
-            if idx == 1:
-                kopeck = "0" + kopeck
-        new_price += (f'{ruble} руб {kopeck} коп ')
-print(new_price)
+        ruble, kopeck = price_parts
+        if len(kopeck) == 1:
+            kopeck = "0" + kopeck
+    _new_price = f'{ruble} руб {kopeck} коп'
+    new_prices.append(_new_price)
+print("answer A")
+print(", ".join(new_prices))
 
-print(list(price_of_goods_list.sort()))
+id_1 = id(new_prices)
 
+new_prices.sort(key = lambda price : [int(price.split(" ")[0]), int(price.split(" ")[2])])
+print("answer B")
+print(new_prices)
+print(id_1 == id(new_prices))
 
+new_prices_decrease = sorted(new_prices, key = lambda price : [int(price.split(" ")[0]), int(price.split(" ")[2])], reverse=True)
+print("answer C")
+print(new_prices_decrease)
+
+print("answer D")
+print(new_prices_decrease[:6])
